a7cd4dc586a887c091cfa4d0bd8bb08c3d17f93b
Alexandr <alexandrdevmisc@yandex.com>
2026-09-16 21:19:43 +0300

    fixes

--- js/blog.js
+++ js/blog.js
@@ -1,12 +1,12 @@
 const container = document.querySelector('.blog-list-content');
 
 container.addEventListener('wheel', (event) => {
-  // Prevent the default vertical page scroll behavior
-  event.preventDefault();
-  
-  // Use scrollBy to move horizontally instead of vertically
-  container.scrollBy({
-    left: event.deltaY,
-    behavior: 'smooth' // Adds an elegant smooth transition
-  });
+    // Prevent the default vertical page scroll behavior
+    event.preventDefault();
+
+    // Use scrollBy to move horizontally instead of vertically
+    container.scrollBy({
+        left: event.deltaY,
+        behavior: 'smooth' // Adds an elegant smooth transition
+    });
 });
